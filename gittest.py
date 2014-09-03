5d51891fb0046c5cb134cfb8e8612760e07561dd
wang server <wo190810401@gmail.com>
2014-09-03 22:49:57 +0800

    还在学习git

---
+++ gittest.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+#-*- coding:utf-8 -*-
+#Filename: gittest.py
+#Author: wangyu190810
+#E-mail: wo190810401@gmail.com
+#Date: 2014-09-03
+#Description: 
+
+import os
+
---
+++ gittest2.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+#-*- coding:utf-8 -*-
+#Filename: gittest2.py
+#Author: wangyu190810
+#E-mail: wo190810401@gmail.com
+#Date: 2014-09-03
+#Description: 
+
+import 
+asd
+f
+as
+df
+asdf
+
